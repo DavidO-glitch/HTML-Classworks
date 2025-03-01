2eace10c93190fd817956ac97a8bd94ef9d62d91
DavidO-glitch <aoomole1@gmail.com>
2025-03-01 20:08:32 +0100

    Add files via upload

---
+++ Percentage.py
@@ -0,0 +1,11 @@
+subject1= 96
+subject2= 93
+subject3= 89
+subject4= 90
+subject5= 85
+
+Totalmarks= subject1+subject2+subject3+subject4+subject5
+Maximumpossiblemark= 100*5
+
+percentage= Totalmarks/Maximumpossiblemark * 100
+print(f"The percentage of all subjects is {percentage} % ") 
